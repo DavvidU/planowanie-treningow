79c792128f64235febedd918175767d9269d7a77
Dawid Ugniewski <80782@student.pb.edu.pl>
2024-01-27 06:30:18 +0100

    Dodanie pokazywania/ukrywania ćwiczenia na liście ćwiczeń

--- src/cwiczenia/ListaCwiczen/ListaCwiczen.tsx
+++ src/cwiczenia/ListaCwiczen/ListaCwiczen.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Cwiczenie } from "../../models/models";
 import CwiczenieSzczegoly from "../CwiczenieSzczegoly/CwiczenieSzczegoly";
 
@@ -16,11 +16,25 @@ function ListaCwiczen({ cwiczenia, wprowadzonyTekst }: ListaCwiczenProps & {wpro
     else
         przefiltrowaneCwiczenia = cwiczenia;
 
+    const [aktywneCwiczenie, setAktywneCwiczenie] = useState<string | null>(null);
+
+    const toggleCwiczenie = (nazwa: string) => {
+        if (aktywneCwiczenie === nazwa) {
+            setAktywneCwiczenie(null);
+        } else {
+            setAktywneCwiczenie(nazwa);
+        }
+    };
+
     return(
       <div>
         {przefiltrowaneCwiczenia.map(cwiczenie => (
           <div className='cwiczenie-item' key={cwiczenie.nazwa}>
-            <CwiczenieSzczegoly nazwa={cwiczenie.nazwa} partia={cwiczenie.partia} opis={cwiczenie.opis}/>
+            <button onClick={() => toggleCwiczenie(cwiczenie.nazwa)}>
+                        {cwiczenie.nazwa}
+            </button>
+            { aktywneCwiczenie == cwiczenie.nazwa && 
+            <CwiczenieSzczegoly nazwa={cwiczenie.nazwa} partia={cwiczenie.partia} opis={cwiczenie.opis}/> }
           </div>
         ))}
       </div>
